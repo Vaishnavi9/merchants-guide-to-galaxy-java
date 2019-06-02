da6936e6ea01a1473de41346e413e9f00a148f97
Vaishnavi9 <vaishnavidhanwade.vd@gmail.com>
2019-06-02 11:57:15 +0530

    expression list derived

--- roman-to-decimal/RomanToDecimal/src/processInput/ProcessInput.java
+++ roman-to-decimal/RomanToDecimal/src/processInput/ProcessInput.java
@@ -15,24 +15,28 @@ public class ProcessInput {
 	
 	
 	//Main list to be processed..
-	static List<String[]> split=new ArrayList<String[]>();
+	static List<String[]> splitInputValues=new ArrayList<String[]>();
+	//mapping romans to galactic values
 	static Map<String, String> mapOfGalacticValues=new HashMap<String,String>();
-	
+	//conatins only the expression string array
+	static List<String[]> expressions=new ArrayList<String[]>(); 
 	
 		
 		public static void main(String[] args) 
 		  { 
 		    List l = readFileInList("C:\\Project Area\\Merchants-Guide-To-Galaxy-Java\\merchants-guide-to-galaxy-java\\roman-to-decimal\\RomanToDecimal\\input.txt"); 
 		    
-		    split=processTheValuesOfArrays(l);
+		    splitInputValues=processTheValuesOfArrays(l);
 		    
-		    mapOfGalacticValues=galacticValuesToRomans(split);
+		    mapOfGalacticValues=galacticValuesToRomans(splitInputValues);
 		    
-		    
+		    expressions=findExpressions(splitInputValues);
 		    
 		    /*Iterator<String> itr = l.iterator();
 		    while (itr.hasNext()) 
-		      System.out.println(itr.next());*/ 
+		      System.out.println(itr.next());*/
+		    
+		    System.out.println(expressions.size());
 		  } 
 	
 		
@@ -56,7 +60,6 @@ public class ProcessInput {
 		//processing the array list by reading the file 
 		public static List<String[]>  processTheValuesOfArrays(List<String> listOfLines) {
 			
-			
 			List<String[]> tempSplit=new ArrayList<String[]>();
 			
 			//converting listOfLines into an arrays of strings  
@@ -73,19 +76,12 @@ public class ProcessInput {
 		
 		//map the galatic values to romans by putting it in the map
 		public static Map<String, String> galacticValuesToRomans(List<String[]> processList){
-			
 			Map<String, String> galacticToRoman=new HashMap<String, String>();
-			
-			
 			for (String[] strings : processList) {
-				
 				if(strings.length==3 && strings[2].matches("I||V||X||C||L||M")) {
-						
 						galacticToRoman.put(strings[0], strings[2]);
 				}
 			}
-			
-			
 			return galacticToRoman;
 		}
 		
@@ -93,9 +89,25 @@ public class ProcessInput {
 		
 		
 		//Find the value of iron, silver, gold
-		 
+		public static List<String[]> findExpressions(List<String[]> processValues) {
+			List<String[]> expressions=new ArrayList<String[]>();
+			//filtering only those lines that would form expressions
+			for (String[] strings : processValues) {	
+				for(int i=0;i<strings.length;i++) {
+				//check if the line contains number and 
+					if(strings[strings.length-1].contains("Credits")
+					&& strings[i].matches(".*\\d.*")) {
+						expressions.add(strings);	
+						//System.out.println(strings[i]);
+					}
+				}
+			}
+			return expressions;
+		} 
 		
 
+		
+		
 
 
 }
